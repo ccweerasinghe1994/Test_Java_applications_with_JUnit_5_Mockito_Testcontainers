a0b1d2b32bae6de92f54cf332d539fa510a359dc
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-01-03 17:45:45 +0530

    adding test

--- 01_Introduction/Examples/src/main/java/com/cgnexus/Calculator.java
+++ 01_Introduction/Examples/src/main/java/com/cgnexus/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public int integerDivision(int dividend, int divider) throws ArithmeticException {
         return dividend / divider;
     }
+
+    public int integerSubtraction(int num1, int num2) {
+        return num1 - num2;
+    }
 }
--- 01_Introduction/Examples/src/test/java/com/cgnexus/CalculatorTest.java
+++ 01_Introduction/Examples/src/test/java/com/cgnexus/CalculatorTest.java
@@ -1,14 +1,17 @@
 package com.cgnexus;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("Test Math Operations in Calculator class")
 class CalculatorTest {
 
+    @DisplayName("integerDivision() happy path")
     @Test
-    void testIntegerDivision_whenValidValuesAreProvided_shouldReturnExpectedResult() {
+    void testIntegerDivision_WhenValidValuesAreProvided_shouldReturnExpectedResult() {
         //Arrange
         Calculator calculator = new Calculator();
 
@@ -16,12 +19,13 @@ class CalculatorTest {
         int result = calculator.integerDivision(4, 2);
 
         //Assert
-        assertEquals(2, result);
+        assertEquals(2, result, "4 divided by 2 should be 2");
 
     }
 
+    @DisplayName("integerDivision() division by zero should throw ArithmeticException")
     @Test
-    void testIntegerDivision_whenInvalidValuesAreProvided_shouldThrowArithmeticException() {
+    void testIntegerDivision_WhenInvalidValuesAreProvided_shouldThrowArithmeticException() {
         //Arrange
         Calculator calculator = new Calculator();
 
@@ -29,8 +33,9 @@ class CalculatorTest {
         assertThrows(ArithmeticException.class, () -> calculator.integerDivision(4, 0));
     }
 
+    @DisplayName("integerDivision() negative values")
     @Test
-    void testIntegerDivision_whenNegativeValuesAreProvided_shouldReturnExpectedResult() {
+    void testIntegerDivision_WhenNegativeValuesAreProvided_shouldReturnExpectedResult() {
         //Arrange
         Calculator calculator = new Calculator();
 
@@ -40,4 +45,37 @@ class CalculatorTest {
         //Assert
         assertEquals(-2, result);
     }
+
+    @DisplayName("integerSubtraction() happy path")
+    @Test
+    void testIntegerSubtraction__WhenValidValuesAreProvided_shouldReturnExpectedResult() {
+
+//        Arrange = given
+        Calculator calculator = new Calculator();
+
+//        Act = when
+        int result = calculator.integerSubtraction(4, 2);
+
+//        Assert = then
+        assertEquals(2, result, "4 subtracted by 2 should be 2");
+    }
+
+    @DisplayName("integerSubtraction() negative values")
+    @Test
+    void testIntegerSubtraction_WhenNegativeValuesAreProvided_shouldReturnExpectedResult() {
+        //Arrange
+        Calculator calculator = new Calculator();
+        int num1 = -4;
+        int num2 = 2;
+        int expectedResult = -6;
+        String message = "The result of subtracting " + num2 + " from " + num1 + " should be " + expectedResult;
+
+        //Act
+        int result = calculator.integerSubtraction(num1, num2);
+
+        //Assert
+        //by using lambda expression, we can condition the execution of the message until the assertion fails
+        assertEquals(expectedResult, result, () -> message);
+    }
+
 }
